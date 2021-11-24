008f355baa256582c26d2be3eab2b226ef167c03
ledemkam <81742990+ledemkam@users.noreply.github.com>
2021-11-24 13:53:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './pages/About';
 import Home from './pages/Home';
 import News from './pages/News';
 
-import NotFound from './pages/NotFound';
+
 
 const App = () => {
    return (
